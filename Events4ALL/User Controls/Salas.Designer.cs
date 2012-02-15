332376d71cf5766e0efb745dca2723732eb975a2
jmgs88 <jmgs88@gmail.com>
2012-02-15 11:19:15 +0000

    Añadir salas y toma contacto: Nombres pestañas salas

--- Events4ALL/User Controls/Salas.Designer.cs
+++ Events4ALL/User Controls/Salas.Designer.cs
@@ -29,53 +29,53 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabAñadirSala = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabBusquedaSala = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tabAñadirSala.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabAñadirSala);
+            this.tabControl1.Controls.Add(this.tabBusquedaSala);
             this.tabControl1.Location = new System.Drawing.Point(3, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(788, 516);
             this.tabControl1.TabIndex = 1;
             // 
-            // tabPage1
+            // tabAñadirSala
             // 
-            this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(780, 490);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(780, 490);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabAñadirSala.Controls.Add(this.label1);
+            this.tabAñadirSala.Location = new System.Drawing.Point(4, 22);
+            this.tabAñadirSala.Name = "tabAñadirSala";
+            this.tabAñadirSala.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAñadirSala.Size = new System.Drawing.Size(780, 490);
+            this.tabAñadirSala.TabIndex = 0;
+            this.tabAñadirSala.Text = "Añadir";
+            this.tabAñadirSala.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Pericles", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(282, 202);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 62);
+            this.label1.Size = new System.Drawing.Size(149, 55);
             this.label1.TabIndex = 0;
             this.label1.Text = "Salas";
+            // 
+            // tabBusquedaSala
+            // 
+            this.tabBusquedaSala.Location = new System.Drawing.Point(4, 22);
+            this.tabBusquedaSala.Name = "tabBusquedaSala";
+            this.tabBusquedaSala.Padding = new System.Windows.Forms.Padding(3);
+            this.tabBusquedaSala.Size = new System.Drawing.Size(780, 490);
+            this.tabBusquedaSala.TabIndex = 1;
+            this.tabBusquedaSala.Text = "Busqueda";
+            this.tabBusquedaSala.UseVisualStyleBackColor = true;
             // 
             // Salas
             // 
@@ -85,8 +85,8 @@
             this.Name = "Salas";
             this.Size = new System.Drawing.Size(794, 519);
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.tabAñadirSala.ResumeLayout(false);
+            this.tabAñadirSala.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -94,8 +94,8 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabAñadirSala;
+        private System.Windows.Forms.TabPage tabBusquedaSala;
         private System.Windows.Forms.Label label1;
     }
 }
