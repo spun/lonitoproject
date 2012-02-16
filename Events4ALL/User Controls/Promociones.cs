c1a6082ace6216befe13531c94ce8a07ab6d097f
Max.odb@gmail.com <Max.odb@gmail.com@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-02-16 18:09:01 +0000

    Oscar: prediseño de Promociones

--- Events4ALL/User Controls/Promociones.cs
+++ Events4ALL/User Controls/Promociones.cs
@@ -15,5 +15,20 @@ namespace Events4ALL
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
