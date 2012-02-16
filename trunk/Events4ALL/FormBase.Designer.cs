368e8912ea8df9311fcf98d9ebf0f5045e5a4e26
shadowfiltro@gmail.com <shadowfiltro@gmail.com@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-02-16 12:40:25 +0000

    Adecuado el tamaño minimo de la ventana

--- trunk/Events4ALL/FormBase.Designer.cs
+++ trunk/Events4ALL/FormBase.Designer.cs
@@ -59,12 +59,12 @@
             this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)));
             this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 30;
             this.listBox1.Location = new System.Drawing.Point(15, 73);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(198, 375);
+            this.listBox1.Size = new System.Drawing.Size(179, 491);
             this.listBox1.TabIndex = 1;
             this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
             this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
@@ -83,7 +83,7 @@
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.panel1.Location = new System.Drawing.Point(15, 34);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(198, 33);
+            this.panel1.Size = new System.Drawing.Size(179, 33);
             this.panel1.TabIndex = 11;
             // 
             // label1
@@ -102,9 +102,9 @@
             this.exitButton.BackColor = System.Drawing.SystemColors.ControlLight;
             this.exitButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.exitButton.Controls.Add(this.label3);
-            this.exitButton.Location = new System.Drawing.Point(15, 508);
+            this.exitButton.Location = new System.Drawing.Point(15, 624);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(198, 37);
+            this.exitButton.Size = new System.Drawing.Size(179, 37);
             this.exitButton.TabIndex = 12;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
@@ -123,9 +123,9 @@
             this.logoutButton.BackColor = System.Drawing.SystemColors.ControlLight;
             this.logoutButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.logoutButton.Controls.Add(this.label2);
-            this.logoutButton.Location = new System.Drawing.Point(15, 467);
+            this.logoutButton.Location = new System.Drawing.Point(15, 583);
             this.logoutButton.Name = "logoutButton";
-            this.logoutButton.Size = new System.Drawing.Size(198, 35);
+            this.logoutButton.Size = new System.Drawing.Size(179, 35);
             this.logoutButton.TabIndex = 13;
             this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
             // 
@@ -142,7 +142,7 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(973, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(1074, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(33, 32);
             this.pictureBox1.TabIndex = 14;
@@ -153,9 +153,9 @@
             this.ventas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.ventas1.Location = new System.Drawing.Point(216, 34);
+            this.ventas1.Location = new System.Drawing.Point(200, 34);
             this.ventas1.Name = "ventas1";
-            this.ventas1.Size = new System.Drawing.Size(790, 519);
+            this.ventas1.Size = new System.Drawing.Size(907, 635);
             this.ventas1.TabIndex = 10;
             // 
             // salas1
@@ -163,9 +163,9 @@
             this.salas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.salas1.Location = new System.Drawing.Point(217, 34);
+            this.salas1.Location = new System.Drawing.Point(201, 34);
             this.salas1.Name = "salas1";
-            this.salas1.Size = new System.Drawing.Size(789, 519);
+            this.salas1.Size = new System.Drawing.Size(906, 635);
             this.salas1.TabIndex = 9;
             // 
             // promociones1
@@ -173,9 +173,9 @@
             this.promociones1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.promociones1.Location = new System.Drawing.Point(217, 34);
+            this.promociones1.Location = new System.Drawing.Point(201, 34);
             this.promociones1.Name = "promociones1";
-            this.promociones1.Size = new System.Drawing.Size(789, 519);
+            this.promociones1.Size = new System.Drawing.Size(906, 635);
             this.promociones1.TabIndex = 8;
             // 
             // logs1
@@ -183,9 +183,9 @@
             this.logs1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.logs1.Location = new System.Drawing.Point(217, 34);
+            this.logs1.Location = new System.Drawing.Point(201, 34);
             this.logs1.Name = "logs1";
-            this.logs1.Size = new System.Drawing.Size(789, 519);
+            this.logs1.Size = new System.Drawing.Size(906, 635);
             this.logs1.TabIndex = 7;
             // 
             // inicio1
@@ -193,9 +193,9 @@
             this.inicio1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.inicio1.Location = new System.Drawing.Point(217, 34);
+            this.inicio1.Location = new System.Drawing.Point(201, 34);
             this.inicio1.Name = "inicio1";
-            this.inicio1.Size = new System.Drawing.Size(789, 519);
+            this.inicio1.Size = new System.Drawing.Size(906, 635);
             this.inicio1.TabIndex = 6;
             // 
             // estadisticas1
@@ -203,9 +203,9 @@
             this.estadisticas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.estadisticas1.Location = new System.Drawing.Point(217, 34);
+            this.estadisticas1.Location = new System.Drawing.Point(201, 34);
             this.estadisticas1.Name = "estadisticas1";
-            this.estadisticas1.Size = new System.Drawing.Size(789, 519);
+            this.estadisticas1.Size = new System.Drawing.Size(906, 635);
             this.estadisticas1.TabIndex = 5;
             // 
             // espectaculos1
@@ -213,9 +213,9 @@
             this.espectaculos1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.espectaculos1.Location = new System.Drawing.Point(217, 34);
+            this.espectaculos1.Location = new System.Drawing.Point(201, 34);
             this.espectaculos1.Name = "espectaculos1";
-            this.espectaculos1.Size = new System.Drawing.Size(789, 519);
+            this.espectaculos1.Size = new System.Drawing.Size(906, 635);
             this.espectaculos1.TabIndex = 4;
             // 
             // clientes1
@@ -223,9 +223,9 @@
             this.clientes1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.clientes1.Location = new System.Drawing.Point(217, 34);
+            this.clientes1.Location = new System.Drawing.Point(201, 34);
             this.clientes1.Name = "clientes1";
-            this.clientes1.Size = new System.Drawing.Size(789, 519);
+            this.clientes1.Size = new System.Drawing.Size(906, 635);
             this.clientes1.TabIndex = 3;
             // 
             // admins1
@@ -233,16 +233,16 @@
             this.admins1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.admins1.Location = new System.Drawing.Point(217, 34);
+            this.admins1.Location = new System.Drawing.Point(201, 34);
             this.admins1.Name = "admins1";
-            this.admins1.Size = new System.Drawing.Size(789, 519);
+            this.admins1.Size = new System.Drawing.Size(906, 635);
             this.admins1.TabIndex = 2;
             // 
             // FormBase
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1018, 557);
+            this.ClientSize = new System.Drawing.Size(1119, 673);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.logoutButton);
             this.Controls.Add(this.exitButton);
