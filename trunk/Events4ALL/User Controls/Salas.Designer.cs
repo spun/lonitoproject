5c5bb3c2b97a443f33613f2797c8f9c62e5f1706
shadowfiltro@gmail.com <shadowfiltro@gmail.com@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-02-16 12:33:26 +0000

    Resueltos varios problemas con la redimensión de la interfaz

--- trunk/Events4ALL/User Controls/Salas.Designer.cs
+++ trunk/Events4ALL/User Controls/Salas.Designer.cs
@@ -38,6 +38,9 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabAñadirSala);
             this.tabControl1.Controls.Add(this.tabBusquedaSala);
             this.tabControl1.Location = new System.Drawing.Point(3, 1);
