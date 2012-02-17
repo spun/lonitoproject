e4a3d554f95e271d917a05451dbdb0df511f9c01
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2012-02-17 19:12:07 +0000

    Nuevo y awesome menu de login Selector de idiomas

--- Events4ALL/FormBase.Designer.cs
+++ Events4ALL/FormBase.Designer.cs
@@ -56,16 +56,10 @@
             // 
             // listBox1
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)));
+            resources.ApplyResources(this.listBox1, "listBox1");
             this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 30;
-            this.listBox1.Location = new System.Drawing.Point(15, 73);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(179, 491);
-            this.listBox1.TabIndex = 1;
             this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
             this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
             // 
@@ -77,172 +71,102 @@
             // 
             // panel1
             // 
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.label1);
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel1.Location = new System.Drawing.Point(15, 34);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(179, 33);
-            this.panel1.TabIndex = 11;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(57, 6);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Menu";
             // 
             // exitButton
             // 
-            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            resources.ApplyResources(this.exitButton, "exitButton");
             this.exitButton.BackColor = System.Drawing.SystemColors.ControlLight;
             this.exitButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.exitButton.Controls.Add(this.label3);
-            this.exitButton.Location = new System.Drawing.Point(15, 624);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(179, 37);
-            this.exitButton.TabIndex = 12;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(75, 12);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(27, 13);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Salir";
             // 
             // logoutButton
             // 
-            this.logoutButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            resources.ApplyResources(this.logoutButton, "logoutButton");
             this.logoutButton.BackColor = System.Drawing.SystemColors.ControlLight;
             this.logoutButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.logoutButton.Controls.Add(this.label2);
-            this.logoutButton.Location = new System.Drawing.Point(15, 583);
             this.logoutButton.Name = "logoutButton";
-            this.logoutButton.Size = new System.Drawing.Size(179, 35);
-            this.logoutButton.TabIndex = 13;
             this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(75, 11);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Logout";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Image = global::Events4ALL.Properties.Resources.update;
-            this.pictureBox1.Location = new System.Drawing.Point(1074, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(33, 32);
-            this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
             // 
             // ventas1
             // 
-            this.ventas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.ventas1.Location = new System.Drawing.Point(200, 34);
+            resources.ApplyResources(this.ventas1, "ventas1");
             this.ventas1.Name = "ventas1";
-            this.ventas1.Size = new System.Drawing.Size(907, 635);
-            this.ventas1.TabIndex = 10;
             // 
             // salas1
             // 
-            this.salas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.salas1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.salas1, "salas1");
             this.salas1.Name = "salas1";
-            this.salas1.Size = new System.Drawing.Size(906, 635);
-            this.salas1.TabIndex = 9;
             // 
             // promociones1
             // 
-            this.promociones1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.promociones1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.promociones1, "promociones1");
             this.promociones1.Name = "promociones1";
-            this.promociones1.Size = new System.Drawing.Size(906, 635);
-            this.promociones1.TabIndex = 8;
             // 
             // logs1
             // 
-            this.logs1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.logs1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.logs1, "logs1");
             this.logs1.Name = "logs1";
-            this.logs1.Size = new System.Drawing.Size(906, 635);
-            this.logs1.TabIndex = 7;
             // 
             // inicio1
             // 
-            this.inicio1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.inicio1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.inicio1, "inicio1");
             this.inicio1.Name = "inicio1";
-            this.inicio1.Size = new System.Drawing.Size(906, 635);
-            this.inicio1.TabIndex = 6;
             // 
             // estadisticas1
             // 
-            this.estadisticas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.estadisticas1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.estadisticas1, "estadisticas1");
             this.estadisticas1.Name = "estadisticas1";
-            this.estadisticas1.Size = new System.Drawing.Size(906, 635);
-            this.estadisticas1.TabIndex = 5;
             // 
             // espectaculos1
             // 
-            this.espectaculos1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.espectaculos1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.espectaculos1, "espectaculos1");
             this.espectaculos1.Name = "espectaculos1";
-            this.espectaculos1.Size = new System.Drawing.Size(906, 635);
-            this.espectaculos1.TabIndex = 4;
             // 
             // clientes1
             // 
-            this.clientes1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.clientes1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.clientes1, "clientes1");
             this.clientes1.Name = "clientes1";
-            this.clientes1.Size = new System.Drawing.Size(906, 635);
-            this.clientes1.TabIndex = 3;
             // 
             // admins1
             // 
-            this.admins1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.admins1.Location = new System.Drawing.Point(201, 34);
+            resources.ApplyResources(this.admins1, "admins1");
             this.admins1.Name = "admins1";
-            this.admins1.Size = new System.Drawing.Size(906, 635);
-            this.admins1.TabIndex = 2;
             // 
             // FormBase
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1119, 673);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.logoutButton);
             this.Controls.Add(this.exitButton);
@@ -258,8 +182,6 @@
             this.Controls.Add(this.admins1);
             this.Controls.Add(this.listBox1);
             this.Name = "FormBase";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Events4ALL";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.exitButton.ResumeLayout(false);
--- Events4ALL/Login.Designer.cs
+++ Events4ALL/Login.Designer.cs
@@ -28,82 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.loginButton = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.textBoxUser = new System.Windows.Forms.TextBox();
             this.textBoxPass = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.idiomas = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // loginButton
-            // 
-            this.loginButton.Location = new System.Drawing.Point(93, 80);
-            this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(75, 23);
-            this.loginButton.TabIndex = 0;
-            this.loginButton.Text = "Login";
-            this.loginButton.UseVisualStyleBackColor = true;
-            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // textBoxUser
             // 
-            this.textBoxUser.Location = new System.Drawing.Point(93, 32);
+            resources.ApplyResources(this.textBoxUser, "textBoxUser");
             this.textBoxUser.Name = "textBoxUser";
-            this.textBoxUser.Size = new System.Drawing.Size(144, 20);
-            this.textBoxUser.TabIndex = 1;
             // 
             // textBoxPass
             // 
-            this.textBoxPass.Location = new System.Drawing.Point(93, 54);
+            resources.ApplyResources(this.textBoxPass, "textBoxPass");
             this.textBoxPass.Name = "textBoxPass";
-            this.textBoxPass.PasswordChar = '•';
-            this.textBoxPass.Size = new System.Drawing.Size(144, 20);
-            this.textBoxPass.TabIndex = 2;
             this.textBoxPass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPass_KeyDown);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 39);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Usuario";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 61);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Contraseña";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Name = "panel1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label4.Name = "label4";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label3.Name = "label3";
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.BackColor = System.Drawing.Color.OliveDrab;
+            this.panel2.Controls.Add(this.loginButton);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.textBoxPass);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBoxUser);
+            this.panel2.Name = "panel2";
+            // 
+            // loginButton
+            // 
+            resources.ApplyResources(this.loginButton, "loginButton");
+            this.loginButton.Name = "loginButton";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // idiomas
+            // 
+            this.idiomas.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("idiomas.ImageStream")));
+            this.idiomas.TransparentColor = System.Drawing.Color.Transparent;
+            this.idiomas.Images.SetKeyName(0, "Spain.png");
+            this.idiomas.Images.SetKeyName(1, "United_Kingdom.png");
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(248, 114);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxPass);
-            this.Controls.Add(this.textBoxUser);
-            this.Controls.Add(this.loginButton);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.MaximizeBox = false;
             this.Name = "Login";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Events4ALL Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button loginButton;
         private System.Windows.Forms.TextBox textBoxUser;
         private System.Windows.Forms.TextBox textBoxPass;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ImageList idiomas;
+        private System.Windows.Forms.Button loginButton;
     }
 }
