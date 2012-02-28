ebbc4a3cc1e3846665d3132066fbb8a194f74c77
Max.odb@gmail.com <Max.odb@gmail.com@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-02-28 19:22:41 +0000

    Finalizada la beta 2 de Promociones. Añadido icono de "mas.png"

--- Events4ALL/User Controls/Promociones.cs
+++ Events4ALL/User Controls/Promociones.cs
@@ -16,39 +16,49 @@ namespace Events4ALL
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void checkBox_MC_ActivarCond1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox_MC_ActivarCond1.Checked == true)
+            {
+                textBox_MC_VC_Valor2.Enabled = true;
+                comboBox_MC_VC_Comparacion2.Enabled = true;
+                comboBox_MC_VC_Tcondicion2.Enabled = true;
+                radioButton_MC_TE2_Cine.Enabled = true;
+                radioButton_MC_TE2_Espectaculo.Enabled = true;
+                radioButton_MC_TE2_Teatro.Enabled = true;
+            }
+            else
+            {
+                textBox_MC_VC_Valor2.Enabled = false;
+                comboBox_MC_VC_Comparacion2.Enabled = false;
+                comboBox_MC_VC_Tcondicion2.Enabled = false;
+                radioButton_MC_TE2_Cine.Enabled = false;
+                radioButton_MC_TE2_Espectaculo.Enabled = false;
+                radioButton_MC_TE2_Teatro.Enabled = false;
+            }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void checkBox_MC_ActivarCond2_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox_MC_ActivarCond2.Checked == true)
+            {
+                textBox_MC_VC_Valor3.Enabled = true;
+                comboBox_MC_VC_Comparacion3.Enabled = true;
+                comboBox_MC_VC_Tcondicion3.Enabled = true;
+                radioButton_MC_TE3_Cine.Enabled = true;
+                radioButton_MC_TE3_Espectaculo.Enabled = true;
+                radioButton_MC_TE3_Teatro.Enabled = true;
+            }
+            else
+            {
+                textBox_MC_VC_Valor3.Enabled = false;
+                comboBox_MC_VC_Comparacion3.Enabled = false;
+                comboBox_MC_VC_Tcondicion3.Enabled = false;
+                radioButton_MC_TE3_Cine.Enabled = false;
+                radioButton_MC_TE3_Espectaculo.Enabled = false;
+                radioButton_MC_TE3_Teatro.Enabled = false;
+            }
         }
 
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_2(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView_PE_listado_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void label1_Click_3(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_4(object sender, EventArgs e)
-        {
-
-        }
     }
 }
