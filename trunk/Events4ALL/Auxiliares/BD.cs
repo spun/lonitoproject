7bbf8f6bbde779e19255dbe4a0e8acfd1766a351
shadowfiltro <shadowfiltro@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-03-12 21:32:27 +0000

    Ahora si :)

---
+++ trunk/Events4ALL/Auxiliares/BD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Events4ALL.Auxiliares
+{
+    public class BD
+    {
+        public BD()
+        {
+
+        }
+
+        public SqlConnection Connect()
+        {
+            string dbDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\Events4ALL\Events4ALL\DB\Events4AllDB.mdf";
+            string s = "Data Source=.\\SQLEXPRESS;AttachDbFilename=" + dbDirectory + ";Integrated Security=True;Connect Timeout=30;User Instance=True";
+            SqlConnection c = new SqlConnection(s);
+            return c;
+        }
+    }
+}
