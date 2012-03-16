887e227e1a721e66b0847b4ad518194e367f88eb
michaelirio <michaelirio@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-03-16 19:12:43 +0000

    Incorporacion de la clase Validaciones.cs. Validaciones.cs puede validar los NIF de los clientes. Abuelo eres un cerdo. Incorporacion de la validacion de NIF en admin.

---
+++ Events4ALL/Auxiliares/Validaciones.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Events4ALL.Auxiliares
+{
+    public class Validaciones
+    {
+        public Validaciones() 
+        { }
+
+        public bool CompruebaNIF(string nif)
+        {
+            bool error = false;
+
+            // el NIF debe de tener un tamaño igual a 9
+            if( nif.Length == 9 )
+            {
+                // extraigo el numero del NIF introducido.
+                Int32 numeros = DevuelveNumero(nif);
+
+                // si hay una letra en medio del NIF, devuelve 0
+                if (numeros != 0)
+                {
+                    // extraigo la letra del NIF introducido.
+                    char letra = DevuelveLetra(nif);
+
+                    // obtengo la letra a partir del numero introducido.
+                    // si la letra introducida es igual a la letra generada, esta bien.
+                    if (letra == ObtieneLetra(numeros))
+                        error = true;
+                }
+            }
+            else
+                error = false;
+            
+            return error;
+        }
+
+        public Int32 DevuelveNumero(string nif)
+        {
+            Int32 numero = 0;
+
+            Int32.TryParse(nif.Substring(0, 8), out numero);
+
+            return numero;
+        }
+
+        public char DevuelveLetra(string nif)
+        {
+            return nif[8];
+        }
+
+        public char ObtieneLetra(Int32 numero)
+        {
+            Int32 indice = numero % 23;
+            return "TRWAGMYFPDXBNJZSQVHLCKET"[indice];
+        }
+
+    }
+}
--- Events4ALL/User Controls/Admins.cs
+++ Events4ALL/User Controls/Admins.cs
@@ -6,14 +6,48 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Events4ALL.Auxiliares;
 
 namespace Events4ALL
 {
     public partial class Admins : UserControl
     {
+        private Validaciones validar;
+        private bool nif;
+        
         public Admins()
         {
             InitializeComponent();
+
+            validar = new Validaciones();
+            nif = false;
+        }
+
+        private void CompruebaNif(object sender, EventArgs e)
+        {
+            //test1.Text = "" + validar.DevuelveNumero(Admin_Perfil_txtBox_NIF.Text);
+            //test2.Text = "" + validar.DevuelveLetra(Admin_Perfil_txtBox_NIF.Text);
+            //test3.Text = "" + validar.ObtieneLetra(validar.DevuelveNumero(Admin_Perfil_txtBox_NIF.Text));
+
+            // esta vacio
+            if (Admin_Perfil_txtBox_NIF.Text.Length == 0)
+            {
+                errorProviderNif.SetError(Admin_Perfil_Label_NIF, "NIF vacio.");
+                //errorProviderNif.Clear();
+                nif = false;
+            }
+            // esta mal
+            else if (!validar.CompruebaNIF(Admin_Perfil_txtBox_NIF.Text))
+            {
+                errorProviderNif.SetError(Admin_Perfil_Label_NIF, "NIF incorrecto.");
+                nif = false;
+            }
+            // esta bien
+            else
+            {
+                errorProviderNif.Clear();
+                nif = true;
+            }
         }
     }
 }
