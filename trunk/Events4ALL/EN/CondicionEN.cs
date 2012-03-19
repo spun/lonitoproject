f354c7be4d99bce8ed3fb3b53f1b48e319ff9a53
Max.odb@gmail.com <Max.odb@gmail.com@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-03-19 16:07:49 +0000

    Agregado PromocionEN.cs y CondicionEN.cs Al salir del programa me sale este mensaje en el cuadro de depuracion: El subproceso '<Sin nombre>' (0x16f4) terminó con código 0 (0x0). El programa '[5736] Events4ALL.vshost.exe: Administrado (v4.0.30319)' terminó con código 0 (0x0). Es normal?

---
+++ trunk/Events4ALL/EN/CondicionEN.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Events4ALL.EN
+{
+    class CondicionEN
+    {
+        private int idCondicion;
+        private string nombre;
+        private string descripcion;
+        private int tCondicion1;
+        private int comparacion1;
+        private int valor1;
+        private int tEvento1;
+        private int tCondicion2;
+        private int comparacion2;
+        private int valor2;
+        private int tEvento2;
+        private int tCondicion3;
+        private int comparacion3;
+        private int valor3;
+        private int tEvento3;
+
+        private int IdPromocion
+        {
+            get { return idCondicion; }
+            set { idCondicion = value; }
+        }
+
+        private string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string Descipcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        private int TCondicion1
+        {
+            get { return tCondicion1; }
+            set { tCondicion1 = value; }
+        }
+
+        private int Comparacion1
+        {
+            get { return comparacion1; }
+            set { comparacion1 = value; }
+        }
+
+        private int Valor1
+        {
+            get { return valor1; }
+            set { valor1 = value; }
+        }
+
+        private int TEvento1
+        {
+            get { return tEvento1; }
+            set { tEvento1 = value; }
+        }
+
+        private int TCondicion2
+        {
+            get { return tCondicion2; }
+            set { tCondicion2 = value; }
+        }
+
+        private int Comparacion2
+        {
+            get { return comparacion2; }
+            set { comparacion2 = value; }
+        }
+
+        private int Valor2
+        {
+            get { return valor2; }
+            set { valor2 = value; }
+        }
+
+        private int TEvento2
+        {
+            get { return tEvento2; }
+            set { tEvento2 = value; }
+        }
+
+        private int TCondicion3
+        {
+            get { return tCondicion3; }
+            set { tCondicion3 = value; }
+        }
+
+        private int Comparacion3
+        {
+            get { return comparacion3; }
+            set { comparacion3 = value; }
+        }
+
+        private int Valor3
+        {
+            get { return valor3; }
+            set { valor3 = value; }
+        }
+
+        private int TEvento3
+        {
+            get { return tEvento3; }
+            set { tEvento3 = value; }
+        }
+    }
+}
---
+++ trunk/Events4ALL/EN/PromocionEN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Events4ALL.EN
+{
+    class PromocionEN
+    {
+        private int idPromocion;
+        private string nombre;
+        private int descuento;
+
+        public int IdPromocion
+        {
+            get { return idPromocion; }
+            set { idPromocion = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Descuento
+        {
+            get { return descuento; }
+            set { descuento = value; }
+        }
+    }
+}
