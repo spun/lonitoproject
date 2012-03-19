20ba7e56ca5f828571c26464dc117af4119978d7
jmgs88 <jmgs88@gmail.com>
2012-03-19 18:55:59 +0000

    constructor, get y set EN

--- Events4ALL/EN/SalasEN.cs
+++ Events4ALL/EN/SalasEN.cs
@@ -7,6 +7,44 @@ namespace Events4ALL.EN
 {
     class SalasEN
     {
-         //lalalala
+        #region variables
+            private int numSala;
+            private string tipoSala;
+            private int numSecciones;
+            private string descripcion;
+        #endregion
+
+        #region set y get
+            public int NumSala
+            {
+                get { return numSala; }
+                set { numSala = value; }
+            }
+            public int NumSecciones
+            {
+                get { return numSecciones; }
+                set { numSecciones = value; }
+            }
+            public string TipoSala
+            {
+                get { return tipoSala; }
+                set { tipoSala = value; }
+            }
+            public string Descripcion
+            {
+                get { return descripcion; }
+                set { descripcion = value; }
+            }
+        #endregion
+
+        #region constructor
+            public SalasEN(int numSala_c,string tipoSala_c,int numSecciones_c,string descripcion_c)
+            {
+                numSala = numSala_c;
+                tipoSala = tipoSala_c;
+                numSecciones = numSecciones_c;
+                descripcion = descripcion_c;
+            }
+        #endregion
     }
 }
