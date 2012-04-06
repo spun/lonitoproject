74c27d67c6f12b7e789d52b407403ffeaea431f6
shadowfiltro <shadowfiltro@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-04-06 11:20:20 +0000

    Mensajes (Carga, lectura y respuesta)

---
+++ trunk/Events4ALL/CAD/MensajesCAD.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Events4ALL.Auxiliares;
+using System.Data.SqlClient;
+
+namespace Events4ALL.CAD
+{
+    public class MensajesCAD
+    {
+        public MensajesCAD()
+        {
+        }
+
+        public DataSet getMessages()
+        {
+            BD bd = new BD();
+            DataSet mensajes = new DataSet();
+            SqlConnection c = bd.Connect();
+
+            try
+            {
+                c.Open();
+                SqlDataAdapter da = new SqlDataAdapter("select * from Mensaje", c);
+                da.Fill(mensajes);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                c.Close();
+            }
+
+            return mensajes;
+        }
+
+        public DataSet getMessageByID(string id)
+        {
+            BD bd = new BD();
+            DataSet mensaje = new DataSet();
+            SqlConnection c = bd.Connect();
+            try
+            {
+                c.Open();
+                SqlDataAdapter da = new SqlDataAdapter("select * from Mensaje m, Cliente c where m.IDMensaje='"+id+"' and m.Cliente=c.NIF", c);
+                da.Fill(mensaje);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                c.Close();
+            }
+
+            return mensaje;
+        }
+
+        public void setResponse(string ID)
+        {
+            BD bd = new BD();
+            SqlConnection c = bd.Connect();
+            
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("update Mensaje set Estado=0 where IDMensaje='"+ID+"'", c);
+                com.ExecuteNonQuery();
+            }
+            catch
+            {
+            }
+            finally
+            {
+                c.Close();
+            }
+
+        }
+    }
+}
--- trunk/Events4ALL/User Controls/Mensajes.Designer.cs
+++ trunk/Events4ALL/User Controls/Mensajes.Designer.cs
@@ -36,19 +36,20 @@
             this.responseText = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textConsulta = new System.Windows.Forms.TextBox();
+            this.textAsunto = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textApellidos = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.msgGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Asunto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.X = new System.Windows.Forms.DataGridViewImageColumn();
-            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.msgGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label6
@@ -64,13 +65,13 @@
             this.groupBox1.Controls.Add(this.responseText);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textConsulta);
+            this.groupBox1.Controls.Add(this.textAsunto);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textApellidos);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textNombre);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
             // 
@@ -101,19 +102,19 @@
             resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
             // 
-            // textBox4
+            // textConsulta
             // 
-            resources.ApplyResources(this.textBox4, "textBox4");
-            this.textBox4.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
+            resources.ApplyResources(this.textConsulta, "textConsulta");
+            this.textConsulta.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textConsulta.Name = "textConsulta";
+            this.textConsulta.ReadOnly = true;
             // 
-            // textBox3
+            // textAsunto
             // 
-            resources.ApplyResources(this.textBox3, "textBox3");
-            this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
+            resources.ApplyResources(this.textAsunto, "textAsunto");
+            this.textAsunto.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textAsunto.Name = "textAsunto";
+            this.textAsunto.ReadOnly = true;
             // 
             // label3
             // 
@@ -125,43 +126,59 @@
             resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
             // 
-            // textBox2
+            // textApellidos
             // 
-            resources.ApplyResources(this.textBox2, "textBox2");
-            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
+            resources.ApplyResources(this.textApellidos, "textApellidos");
+            this.textApellidos.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textApellidos.Name = "textApellidos";
+            this.textApellidos.ReadOnly = true;
             // 
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
             // 
-            // textBox1
+            // textNombre
             // 
-            resources.ApplyResources(this.textBox1, "textBox1");
-            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
+            resources.ApplyResources(this.textNombre, "textNombre");
+            this.textNombre.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textNombre.Name = "textNombre";
+            this.textNombre.ReadOnly = true;
             // 
-            // dataGridView1
+            // msgGridView
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            resources.ApplyResources(this.dataGridView1, "dataGridView1");
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.ColumnHeadersVisible = false;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.msgGridView.AllowUserToAddRows = false;
+            this.msgGridView.AllowUserToDeleteRows = false;
+            this.msgGridView.AllowUserToResizeColumns = false;
+            this.msgGridView.AllowUserToResizeRows = false;
+            resources.ApplyResources(this.msgGridView, "msgGridView");
+            this.msgGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.msgGridView.BackgroundColor = System.Drawing.Color.White;
+            this.msgGridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.msgGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.msgGridView.ColumnHeadersVisible = false;
+            this.msgGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
             this.Asunto,
             this.X});
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
+            this.msgGridView.Name = "msgGridView";
+            this.msgGridView.ReadOnly = true;
+            this.msgGridView.RowHeadersVisible = false;
+            this.msgGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.msgGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.msgGridView_CellClick);
+            // 
+            // dataGridViewImageColumn1
+            // 
+            this.dataGridViewImageColumn1.FillWeight = 10.15228F;
+            resources.ApplyResources(this.dataGridViewImageColumn1, "dataGridViewImageColumn1");
+            this.dataGridViewImageColumn1.Image = global::Events4ALL.Properties.Resources.delete;
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            // 
+            // ID
+            // 
+            resources.ApplyResources(this.ID, "ID");
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
             // 
             // Asunto
             // 
@@ -178,24 +195,17 @@
             this.X.Name = "X";
             this.X.ReadOnly = true;
             // 
-            // dataGridViewImageColumn1
-            // 
-            this.dataGridViewImageColumn1.FillWeight = 10.15228F;
-            resources.ApplyResources(this.dataGridViewImageColumn1, "dataGridViewImageColumn1");
-            this.dataGridViewImageColumn1.Image = global::Events4ALL.Properties.Resources.delete;
-            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
-            // 
             // Mensajes
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.msgGridView);
             this.Name = "Mensajes";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.msgGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,17 +219,18 @@
         private System.Windows.Forms.Button mailButton;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textConsulta;
+        private System.Windows.Forms.TextBox textAsunto;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textApellidos;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Asunto;
-        private System.Windows.Forms.DataGridViewImageColumn X;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.DataGridView msgGridView;
         private System.Windows.Forms.Label statusLabel;
         public System.Windows.Forms.TextBox responseText;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Asunto;
+        private System.Windows.Forms.DataGridViewImageColumn X;
     }
 }
