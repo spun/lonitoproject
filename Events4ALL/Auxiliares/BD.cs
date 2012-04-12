817733780b5515b3a40f55b3a460d94dd0e481fd
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2012-04-12 09:37:45 +0000

    Eliminado de raiz el origen de los problemas en FormBase.cs

--- Events4ALL/Auxiliares/BD.cs
+++ Events4ALL/Auxiliares/BD.cs
@@ -17,7 +17,9 @@ namespace Events4ALL.Auxiliares
 
         public SqlConnection Connect()
         {
-            string s = "Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|Events4AllDB.mdf ;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData));
+            Console.WriteLine("#########Datadirectory es: "+AppDomain.CurrentDomain.GetData("DataDirectory"));
+            string s = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|Events4AllDB.mdf ;Integrated Security=True;User Instance=True";
             SqlConnection c = new SqlConnection(s);
             return c;
         }
--- Events4ALL/Program.cs
+++ Events4ALL/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Events4ALL
 {
@@ -13,6 +14,11 @@ namespace Events4ALL
         [STAThread]
         static void Main()
         {
+            string sourcePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string targetPath =  System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+            string sourceFile = System.IO.Path.Combine(sourcePath, "Events4AllDB.mdf");
+            string destFile = System.IO.Path.Combine(targetPath, "Events4AllDB.mdf");
+            System.IO.File.Copy(sourceFile, destFile, true);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
