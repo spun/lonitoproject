e0b5c5dfda94d0955e6f447e1a03b56fdd70ec32
shadowfiltro <shadowfiltro@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-04-18 14:29:33 +0000

    Solucionado un bug de la muerte en estadisticas

--- trunk/Events4ALL/User Controls/Estadisticas.cs
+++ trunk/Events4ALL/User Controls/Estadisticas.cs
@@ -48,7 +48,7 @@ namespace Events4ALL
             numerosMeses = new Dictionary<string, int>()
             {
                 {"Enero", 1}, {"Febrero", 2}, {"Marzo", 3}, {"Abril", 4}, {"Mayo", 5}, {"Junio", 6},
-                {"julio", 7}, {"Agosto", 8}, {"Septiembre", 9}, {"Octubre", 10}, {"Noviembre", 11}, {"Diciembre", 12}
+                {"Julio", 7}, {"Agosto", 8}, {"Septiembre", 9}, {"Octubre", 10}, {"Noviembre", 11}, {"Diciembre", 12}
             };
 
             comboMes.Text = "Junio";
