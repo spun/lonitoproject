14616a94aecd0d8148d192f79e866bd8f8c7c1b0
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2012-04-27 17:44:08 +0000

    Calendario de cartelera fuera por feo

--- WEvents4ALL/cartelera.aspx.designer.cs
+++ WEvents4ALL/cartelera.aspx.designer.cs
@@ -37,13 +37,4 @@ public partial class  {
     /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     /// </remarks>
     protected global::System.Web.UI.WebControls.Button Button3;
-    
-    /// <summary>
-    /// Control Calendar1.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.Calendar Calendar1;
 }
