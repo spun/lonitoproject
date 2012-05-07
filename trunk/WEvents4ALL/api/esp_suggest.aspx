ca19dfdf504ea2135fb335bcc430cbc8c68dc6a9
victornmatrix <victornmatrix@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-05-07 11:05:01 +0000

    -Cambiados parametros del slide de inicio. -Los espectaculos ahora cargan salas con datos reales. -Cambiado qtip por popover en la seleccion de asientos. -Añadido buscador con sugerencias a la barra.

---
+++ trunk/WEvents4ALL/api/esp_suggest.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="esp_suggest.aspx.cs" Inherits="WEvents4ALL.api.esp_suggest" %>
+<%= sJson %>
---
+++ trunk/WEvents4ALL/api/sala_data.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using Entities;
+using System.Data;
+
+namespace WEvents4ALL.api
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public string sJson = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {            
+            SalasEN salaEn = new SalasEN();
+            DataSet salaRecuperar = new DataSet();
+
+            try
+            {
+                string idSalaIN = Request.QueryString["id"];
+                salaRecuperar = salaEn.RecuperarSala(idSalaIN);
+                int nSecciones = Convert.ToInt16(salaRecuperar.Tables[0].Rows[0][3].ToString());
+
+                Dictionary<string, object> dict = new Dictionary<string, object>();
+                dict.Add("idSala", idSalaIN);
+                dict.Add("numSecciones", nSecciones);
+
+                object[] secciones = new object[nSecciones];
+
+                for (int i = 0; i < nSecciones; i++)
+                {
+                    Dictionary<string, object> seccData = new Dictionary<string, object>();
+                    seccData.Add("filas", salaRecuperar.Tables[0].Rows[i][6].ToString());
+                    seccData.Add("columnas", salaRecuperar.Tables[0].Rows[i][7].ToString());
+
+                    secciones.SetValue(seccData, i);
+                }
+                dict.Add("secciones", secciones);
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                sJson = serializer.Serialize((object)dict);
+
+
+            }
+            catch (Exception ex)
+            {
+                sJson = "error";
+            }
+        }
+    }
+}
