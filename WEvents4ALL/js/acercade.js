452dc3756258fcf734abcccdc928a382caad8ca1
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2012-05-16 14:15:50 +0000

    Mapa de Google Maps incrustrado en "acerca de"

---
+++ WEvents4ALL/js/acercade.js
@@ -0,0 +1,12 @@
+﻿var map;
+function initialize() {
+    var myOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(-34.397, 150.644),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map_canvas'),
+        myOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
