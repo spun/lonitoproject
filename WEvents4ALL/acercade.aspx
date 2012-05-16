bf0b0a2bbeb0126ac5a49b0e73df38310a636b2a
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2012-05-16 16:30:42 +0000

    Actualizado el acerca de con geolocalizacion automática y trazado de rutas.

--- WEvents4ALL/acercade.aspx
+++ WEvents4ALL/acercade.aspx
@@ -9,19 +9,25 @@
     <div class="row">
         <div class="span4">
             <div id="text">
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
-                <p>Nyan nyan nyan nyan</p>
+                <h3>La Llotja</h3>
+                <p>La Llotja Sala Cultural es un espacio cultural, inaugurado en 2008, que ocupa una gran parte del edificio de la antigua lonja de frutas y verduras (El Prao) del barrio de Altabix, tras la reforma de este edificio construido entre los años 1941 y 1942.</p>
+                <h3>Localización</h3>
+                <p>Plaça de la Llotja</p>
+                <p>03202 Elche - Alicante</p>
             </div>
         </div>
         <div class="span5">
-            <div id="map_canvas"></div>
+            <div id="mapa_content"></div>
+        </div>
+    </div>
+</div>
+
+<h2>¿Pero como llego?</h2>
+<div class="well">
+    <div class="row">
+        <div class="span10">
+            <div id="mapa_content2"></div>
+            <button class="btn btn-primary" id="geoloc" type="button"><i class="icon-road icon-white"></i> Llévame </button>
         </div>
     </div>
 </div>
--- WEvents4ALL/js/acercade.js
+++ WEvents4ALL/js/acercade.js
@@ -1,12 +1,112 @@
 ﻿var map;
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 function initialize() {
+
+
+    directionsDisplay = new google.maps.DirectionsRenderer();
+
+    var myLatlng = new google.maps.LatLng(38.270142, -0.686175);
+
     var myOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 13,
+        center: new google.maps.LatLng(38.267985, -0.699168),
+        disableDefaultUI: true,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById('map_canvas'),
-        myOptions);
+    map = new google.maps.Map(document.getElementById('mapa_content'), myOptions);
+
+    var myOptions2 = {
+        zoom: 15,
+        center: myLatlng,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map2 = new google.maps.Map(document.getElementById('mapa_content2'), myOptions2);
+    directionsDisplay.setMap(map2);
+    directionsDisplay.setOptions({ suppressMarkers: true });
+
+    var markerLoc = new google.maps.Marker({
+        position: myLatlng,
+        map: map2,
+        title: "Events4All",
+        icon: 'http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-eda11d/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/theater.png'
+    });
+
+
+    $("#geoloc").click(function () {
+        // Try W3C Geolocation (Preferred)
+        if (navigator.geolocation) {
+            browserSupportFlag = true;
+            navigator.geolocation.getCurrentPosition(function (position) {
+                initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                var start = initialLocation;
+                var end = myLatlng;
+                var mapBounds = new google.maps.LatLngBounds();
+                map2.fitBounds(mapBounds);
+                mapBounds.extend(initialLocation);
+                mapBounds.extend(myLatlng);
+                map2.setCenter(initialLocation);
+                var markerUser = new google.maps.Marker({
+                    position: initialLocation,
+                    map: map2,
+                    title: "Tu",
+                    icon: 'http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-ffc11f/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/male-2.png'
+                });
+                var request = {
+                    origin: start,
+                    destination: end,
+                    travelMode: google.maps.TravelMode.DRIVING
+                };
+                directionsService.route(request, function (result, status) {
+                    if (status == google.maps.DirectionsStatus.OK) {
+                        directionsDisplay.setDirections(result);
+                    }
+                });
+            }, function () {
+                handleNoGeolocation(browserSupportFlag);
+            });
+            // Try Google Gears Geolocation
+        } else if (google.gears) {
+            browserSupportFlag = true;
+            var geo = google.gears.factory.create('beta.geolocation');
+            geo.getCurrentPosition(function (position) {
+                initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                var start = initialLocation;
+                var end = myLatlng;
+                var mapBounds = new google.maps.LatLngBounds();
+                map2.fitBounds(mapBounds);
+                mapBounds.extend(initialLocation);
+                mapBounds.extend(myLatlng);
+                map2.setCenter(initialLocation);
+                var markerUser = new google.maps.Marker({
+                    position: initialLocation,
+                    map: map2,
+                    title: "Tu",
+                    icon: 'http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-ffc11f/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/male-2.png'
+                });
+                var request = {
+                    origin: start,
+                    destination: end,
+                    travelMode: google.maps.TravelMode.DRIVING
+                };
+                directionsService.route(request, function (result, status) {
+                    if (status == google.maps.DirectionsStatus.OK) {
+                        directionsDisplay.setDirections(result);
+                    }
+                });
+            }, function () {
+                handleNoGeoLocation(browserSupportFlag);
+            });
+            // Browser doesn't support Geolocation
+        } else {
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
+            alert("No soporta geoloc");
+        }
+
+
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
