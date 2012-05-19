1e0248c210c1a754c40048a5745a90fb138796a3
PredatorHunter.NT@gmail.com <PredatorHunter.NT@gmail.com@63a6bbc3-0ee7-d800-09fe-bf6d4b58c3c5>
2012-05-19 21:58:33 +0000

    Registro de Usuario. Aparece en la BD, pero una vez registrado, no consigo loguearme con dicho usuario. Falta la carga de la imagen.

--- WEvents4ALL/registro.aspx.cs
+++ WEvents4ALL/registro.aspx.cs
@@ -4,13 +4,71 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Entities;
+using Auxiliares;
+using System.Data;
+using System.Text.RegularExpressions;
 
 namespace WEvents4ALL
 {
     public partial class registro : System.Web.UI.Page
     {
+        public ClientesEN en_cliente;
+        public DataSet fichaCliente = new DataSet();
+        bool introducido;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        // Registro el usuario en la BD
+        protected void Registro_Cliente(object sender, EventArgs e)
+        {
+            ClientesEN nuevoCliente = new ClientesEN();
+            MultiView mvc = (MultiView)Master.FindControl("MultiViewAlerts");
+            mvc.ActiveViewIndex = 0;
+            Label lbTittle = (Label)Master.FindControl("errorViewTitle");
+            Label lbMsg = (Label)Master.FindControl("errorViewMsg");
+
+            nuevoCliente.DNI = textbox_NIFC.Text;
+            nuevoCliente.Nombre = textbox_nombreC.Text;
+            nuevoCliente.Apellidos = textbox_apellidosC.Text;
+            nuevoCliente.Pais = DropDownList_Pais.Text;
+            nuevoCliente.Provincia = DropDownList_Prov.Text;
+            nuevoCliente.Localidad = textbox_localidadC.Text;
+            nuevoCliente.CP = textbox_CodigoPostalC.Text;
+            nuevoCliente.Movil = textbox_movilC.Text;
+            nuevoCliente.Telefono = textbox_telefonoC.Text;
+            nuevoCliente.Mail = textbox_emailC.Text;
+            nuevoCliente.Fecha = Convert.ToDateTime(textbox_fechaN.Text);
+            nuevoCliente.Nick = textbox_usuario.Text;
+            nuevoCliente.Password = textbox_pass.Text;
+
+            if (DropDownList_Sexo.Text == "Hombre")
+                nuevoCliente.Sexo = 0;
+            else if (DropDownList_Sexo.Text == "Mujer")
+                nuevoCliente.Sexo = 1;
+            else
+                nuevoCliente.Sexo = -1;
+
+            introducido = nuevoCliente.InsertarCliente();
+
+            if (introducido == true)
+            {
+                //MessageBox.Show("Los datos se han introducido satisfactoriamente.");
+                //Response.StatusCode = 404;
+                lbTittle.Text = "OK - TODO CORRECTO";
+                lbMsg.Text = "Datos introducidos con exito";
+                
+            }
+            else if (introducido == false)
+            {
+                //MessageBox.Show("Error. Se ha producido un fallo cuando se intentaba insertar el Cliente.");
+                lbTittle.Text = "ERROR - FALLO";
+                lbMsg.Text = "Ocurrió un error al insertar los datos";
+                
+            }
 
         }
     }
--- WEvents4ALL/registro.aspx.designer.cs
+++ WEvents4ALL/registro.aspx.designer.cs
@@ -11,5 +11,158 @@ namespace WEvents4ALL {
     
     
     public partial class registro {
+        
+        /// <summary>
+        /// Control textbox_nombreC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_nombreC;
+        
+        /// <summary>
+        /// Control textbox_apellidosC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_apellidosC;
+        
+        /// <summary>
+        /// Control textbox_NIFC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_NIFC;
+        
+        /// <summary>
+        /// Control textbox_emailC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_emailC;
+        
+        /// <summary>
+        /// Control textbox_fechaN.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_fechaN;
+        
+        /// <summary>
+        /// Control textbox_usuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_usuario;
+        
+        /// <summary>
+        /// Control textbox_pass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_pass;
+        
+        /// <summary>
+        /// Control textbox_rePass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_rePass;
+        
+        /// <summary>
+        /// Control DropDownList_Pais.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList_Pais;
+        
+        /// <summary>
+        /// Control DropDownList_Prov.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList_Prov;
+        
+        /// <summary>
+        /// Control textbox_localidadC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_localidadC;
+        
+        /// <summary>
+        /// Control textbox_domicilioC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_domicilioC;
+        
+        /// <summary>
+        /// Control textbox_CodigoPostalC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_CodigoPostalC;
+        
+        /// <summary>
+        /// Control textbox_telefonoC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_telefonoC;
+        
+        /// <summary>
+        /// Control textbox_movilC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textbox_movilC;
+        
+        /// <summary>
+        /// Control DropDownList_Sexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList_Sexo;
+        
+        /// <summary>
+        /// Control bGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bGuardar;
     }
 }
