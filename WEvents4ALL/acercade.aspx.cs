52ca98d55786a10a6daed8b4e377d570a00b9f18
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2012-05-19 09:41:15 +0000

    Añadidos el mapa de la instalaciones a "Acerca de". Añadidas las imagenes de los marcadores, ya no hay "Hot linking".

--- WEvents4ALL/acercade.aspx.cs
+++ WEvents4ALL/acercade.aspx.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Entities;
+using System.Data;
 
 namespace WEvents4ALL
 {
     public partial class acercade : System.Web.UI.Page
     {
+
+        public SalasEN sEN = new SalasEN();
+        public DataSet salas = new DataSet();
+        public int aforo1 = 0;
+        public int aforo2 = 0;
+        public int aforo3 = 0;
+        public int aforo4 = 0;
+        public int[] aforo = new int[100];
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            int i=1;
+            salas = sEN.getSalas();
+            foreach (DataRow r in salas.Tables[0].Rows)
+            {
+                aforo[i] = (int)r["Aforo"];
+                i++;
+            }
         }
+        
     }
 }
